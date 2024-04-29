c66584dea67263a6febf84279f1ede4494319fd1
WebWithAstha <lodhiastha85@gmail.com>
2024-04-29 10:44:14 +0530

    payment gateway integrated

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var router = express.Router();
 const passport = require('passport');
+const Razorpay = require('razorpay');
+var instance = new Razorpay({
+  key_id: 'rzp_test_6I6FvnUbmGjI5S',
+  key_secret: 'LA3sRQkHGcFo0NzBysyTz6hj',
+});
+
 
 /* GET home page. */
 router.use('/',require('../middlewares/localAuth.js'));
@@ -19,6 +25,16 @@ router.get('/book/slot/:zone/:slot', function(req, res, next) {
 router.get('/slots', function(req, res, next) {
   res.render('slots', { footer:false });
 });
+router.post('/bookorder/:slot',function(req, res, next) {
+  var options = {
+    amount: req.body.amt,  // amount in the smallest currency unit
+    currency: "INR",
+    receipt: "order_rcptid_11"
+  };
+  instance.orders.create(options, function(err, order) {
+    res.send(order)
+  });
+})
 
 router.post('/login', passport.authenticate('local',{
   successRedirect:'/home',
